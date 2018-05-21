791bf34eaeb77a7f3ee0f615f1b1e05a1812c4ee
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-05-21 15:27:47 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,8 @@
+var temperatureincelcius = 40; //variable temperatureincelsius is assigned the value -40
+var celsiustofahrenheit = (temperatureincelcius*9)/5 + 32; //celcius is converted to fahrenheit and stored in the variable celciustofahrenheit
+console.log("Temperature of " + temperatureincelcius + " celsius, will be " + celsiustofahrenheit + " fahrenheit");
+//“Temperature of 40 celsius, will be 104 fahrenheit” is loged into console.
+var temperatureinfahrenheit = 104; //variable temperatureinfahrenheit is assigned the value -104
+var FahrenheittoCelsius = ((temperatureinfahrenheit - 32) * 5) / 9; //fahrenheit is converted to celsius and stored in the variable fahrenheittocelsius
+console.log("Temperature of " + temperatureinfahrenheit + " fahrenheit, will be " + FahrenheittoCelsius + " celsius");
+//“Temperature of 104 fahrenheit, will be 40 celsius” is loged into console.
